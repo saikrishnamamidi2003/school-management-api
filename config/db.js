4e45146010297abb94c66ee082442e23ee38f54e
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-07 01:34:44 +0530

    fix: mysql connection pool for railway

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 db.connect((err) => {
