fbeab50d867ad5894c83d1b9657677f0e9297826
sai krishna <saikrishnamamidi2003@gmail.com>
2026-03-07 02:05:50 +0530

    Fixes in database

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
